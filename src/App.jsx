b956eda5796bbf4a462a97105acfcad72eaaf4c9
Amirshah98760 <amirshah98760@gmail.com>
2024-06-30 15:23:07 +0500

    react todo app

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import './App.css';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, { text: input, completed: false }]);
+      setInput('');
+    }
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="flexContainer">
+    <div className="App">
+      <h1 className='todo-heading'>Todo App</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+import './TodoList.css';
+
+const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
+  return (
+    <ul className="todo-list">
+      {todos.map((todo, index) => (
+        <TodoItem
+          key={index}
+          todo={todo}
+          toggleTodo={() => toggleTodo(index)}
+          deleteTodo={() => deleteTodo(index)}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
